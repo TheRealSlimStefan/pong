63e2ccc66c2294ceaab6f80b7b816fa978ef2dbf
Marcin Stefanowicz <19smarcins98@gmail.com>
2020-09-04 17:34:22 +0200

    another first commit

--- Game.js
+++
@@ -1,6 +0,0 @@
-//game class which will manage whole game
-export default class Game {
-    constructor() {
-
-    }
-}
--- main.js
+++
@@ -1,5 +0,0 @@
-//importing class Game from Game.js
-import Game from '/Game.js';
-
-//creating new game object which will run a game
-const game = new Game();
